fb9e397eb843681adf25eb88f68e300e304c3aa4
Harsh Vyas <harsh.vyas467@gmail.com>
2025-07-10 20:29:36 +0530

    Created a rest controller in microservice B and hit the rest api from microservice A

---
+++ src/main/java/com/microservices/camel_microservice_b/controller/CurrencyExchangeController.java
@@ -0,0 +1,19 @@
+package com.microservices.camel_microservice_b.controller;
+
+import com.microservices.camel_microservice_b.pojo.CurrencyExchange;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange findConversionValue(@PathVariable String from, @PathVariable String to){
+        return new CurrencyExchange(10001,from, to, BigDecimal.TEN);
+    }
+
+
+}
