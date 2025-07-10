542dc07eb88228e6adce644c3dcb5a6c15d6baba
Harsh Vyas <harsh.vyas467@gmail.com>
2025-07-10 18:41:05 +0530

    Kafka reciever route and properties added

---
+++ src/main/java/com/microservices/camel_microservice_b/routes/KafkaJsonReceiverRouter.java
@@ -0,0 +1,29 @@
+package com.microservices.camel_microservice_b.routes;
+
+import com.microservices.camel_microservice_b.pojo.CurrencyExchange;
+import com.microservices.camel_microservice_b.transformer.MyCurrencyExchangeProcessor;
+import com.microservices.camel_microservice_b.transformer.MyCurrencyExchangeTransformer;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaJsonReceiverRouter extends RouteBuilder {
+    @Autowired
+    MyCurrencyExchangeProcessor myCurrencyExchangeProcessor;
+
+    @Autowired
+    MyCurrencyExchangeTransformer myCurrencyExchangeTransformer;
+
+    @Override
+    public void configure() throws Exception {
+        from("kafka:myKafkaTopic")
+                .unmarshal()
+                .json(JsonLibrary.Jackson, CurrencyExchange.class)
+                .bean(myCurrencyExchangeProcessor)
+                .bean(myCurrencyExchangeTransformer)
+                .to("log:received-message-from-kafka");
+
+    }
+}
